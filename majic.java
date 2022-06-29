5b97694987879a162ed0bc6005d0c6a3a30251dc
programmersyed <abrahamsyed10@gmail.com>
2022-06-29 08:38:05 -0400

    Create majic.java

---
+++ majic.java
@@ -0,0 +1,15 @@
+public class Magic {
+	public static void main(String[] args) {
+int myNumber = 1;
+//original number
+int stepOne = 1*1;
+int stepTwo = 1+1;
+int stepThree = 2/1;
+int stepFour = stepThree +17;
+int stepFive = stepFour - 1;
+int stepSix = stepFive/6;
+System.out.println(stepSix);
+		
+
+	}
+}
